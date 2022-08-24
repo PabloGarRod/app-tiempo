76570b62b078b248a27036d82569488ab80a93f6
Pablo <pablogarcirodriguez@gmail.com>
2022-08-24 19:51:31 +0200

    Intentando hacer funcionar bien el botón Madrid

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ class Coordenadas {
    }
  }
 
- const Madrid = new Coordenadas(40.4165, -3.7026)
- const Barcelona = new Coordenadas(41.38879, 2.15899)
- const Sevilla = new Coordenadas(37.38283, -5.97317)
+ const coordMadrid = new Coordenadas(40.4165, -3.7026)
+ const coordBarcelona = new Coordenadas(41.38879, 2.15899)
+ const coordSevilla = new Coordenadas(37.38283, -5.97317)
 
  async function recibirDatos(ciudad){
    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${ciudad.lat}&lon=${ciudad.lon}&appid=ba153d43089f4c0ea95b1e1e900303c5`
@@ -16,33 +16,35 @@ class Coordenadas {
    return datos;
  }
 
-const datosMadrid = await recibirDatos(Madrid);
-const datosBarcelona = await recibirDatos(Barcelona);
-const datosSevilla = await  recibirDatos(Sevilla);
+const madrid = await recibirDatos(coordMadrid);
+const barcelona = await recibirDatos(coordBarcelona);
+const sevilla = await  recibirDatos(coordSevilla);
 
 function kelvinToCelsius(kelvin){
    const celsius = kelvin - 273.15
    return celsius
 }
 
-console.log(`En Madrid hace ${Math.round(kelvinToCelsius(datosMadrid.main.temp))}ºC`);
-console.log(`En Sevilla hace ${Math.round(kelvinToCelsius(datosSevilla.main.temp))}ºC`)
-console.log(`En Barcelona hace ${Math.round(kelvinToCelsius(datosBarcelona.main.temp))}ºC`)
+
+const container = document.getElementById('container')
+
+function mostrarDatos(ciudad){
+   const cuadro = document.createElement('div')
+   const temp = document.createTextNode(`Temperatrura: ${Math.round(kelvinToCelsius(ciudad.main.temp))}º`);
+   cuadro.appendChild(temp)
+   container.appendChild(cuadro)
+}
+
+const botonMadrid = document.getElementById('boton-madrid');
+botonMadrid.addEventListener('click', alert('Hola'))
 
 
 
-//  Madrid: {
-//    lon: -3.7026,
-//    lat: 40.4165
-// },
 
-// Barcelona: {
-//    lon: 2.15899,
-//    lat: 41.38879
-// },
+// console.log(`En Madrid hace ${Math.round(kelvinToCelsius(datosMadrid.main.temp))}ºC`);
+// console.log(`En Sevilla hace ${Math.round(kelvinToCelsius(datosSevilla.main.temp))}ºC`)
+// console.log(`En Barcelona hace ${Math.round(kelvinToCelsius(datosBarcelona.main.temp))}ºC`)
 
-// Sevilla: {
-//    lon: -5.97317,
-//    lat: 37.38283,
-// }
+
+
  
